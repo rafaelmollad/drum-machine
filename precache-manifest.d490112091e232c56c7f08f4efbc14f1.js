d21ded13ee382c5a4221c04421ddf4954aae81ab
Rafael Molla <rafaelmolladouglas@gmail.com>
2019-01-17 10:20:51 -0300

    Updates

---
+++ precache-manifest.d490112091e232c56c7f08f4efbc14f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "66538d0a05ffbae23551",
+    "url": "/static/js/main.66538d0a.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "66538d0a05ffbae23551",
+    "url": "/static/css/main.85d9c91e.chunk.css"
+  },
+  {
+    "revision": "7066236730d5064e338a5d38dd822e89",
+    "url": "/index.html"
+  }
+];
